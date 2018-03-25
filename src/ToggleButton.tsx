9000b3930112e66f3b9ec1c9f8fdabed10086a4c
eric_yip <eric_yip@mail.gss.com.tw>
2018-03-25 16:46:31 +0800

    add toggle button

---
+++ src/ToggleButton.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div`
+  display: inline-block;
+  width: ${(props: Props) => props.size ? `${props.size}px` : '16px'};
+  height: ${props => props.size ? `${props.size}px` : '16px'};
+  vertical-align: middle;
+  cursor: pointer;
+  color: ${props => (props.color ? props.color : '#000')};
+
+  &:hover {
+    color: #999;
+  }
+`;
+
+interface Props {
+  color: string;
+  size: number;
+  hoverColor: string;
+}
+
+const ToggleButton = (props: Props) => (
+  <Root {...props}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path
+        fill="currentColor"
+        // tslint:disable-next-line
+        d="M328 256c0 39.8-32.2 72-72 72s-72-32.2-72-72 32.2-72 72-72 72 32.2 72 72zm104-72c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72zm-352 0c-39.8 0-72 32.2-72 72s32.2 72 72 72 72-32.2 72-72-32.2-72-72-72z"
+      />
+    </svg>
+  </Root>
+);
+
+export default ToggleButton;
--- src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const getElementContentWidth = (element: any) => {
-  const styles = window.getComputedStyle(element);
-  const padding = parseFloat(styles.paddingLeft || '2') +
-      parseFloat(styles.paddingRight || '2');
-  return element.clientWidth - padding;
-};
